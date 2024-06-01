8d987b611634d1217d8f1e05830b2adc0332df81
natabile <codernathailu@gmail.com>
2024-06-01 12:39:12 +0300

    todo commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import TodoList from './componet/todo-list';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
